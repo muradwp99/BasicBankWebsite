c50945822ce0f18fbf96c46aaa8dfa0b35432c31
muradwp99 <muradmd107@gmail.com>
2022-02-12 20:23:55 +0600

    done login

--- banking.js
+++ banking.js
@@ -1,2 +1,12 @@
 //login handler
 
+document.getElementById('login-button').addEventListener('click', function () {
+    const emailField = document.getElementById('email-input');
+    const userEmail = emailField.value;
+    const passField = document.getElementById('password-input');
+    const userPass = passField.value;
+    if (userEmail == 'admin@gmail.com' && userPass == 'okay') {
+        console.log('valid user');
+    }
+
+})
